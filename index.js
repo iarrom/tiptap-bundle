8c4ded2f33aa015d5113adbc18cd5d5a8ab9ef25
Iaroslav Romanov <54136840+iarrom@users.noreply.github.com>
2023-08-06 17:43:29 +0100

    publish tiptap editor

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Editor } from '@tiptap/core';
+import Blockquote from '@tiptap/extension-blockquote';
+import BulletList from '@tiptap/extension-bullet-list';
+import HardBreak from '@tiptap/extension-hard-break';
+import Heading from '@tiptap/extension-heading';
+import ListItem from '@tiptap/extension-list-item';
+import OrderedList from '@tiptap/extension-ordered-list';
+import Paragraph from '@tiptap/extension-paragraph';
+
+window.Editor = Editor;
+window.Blockquote = Blockquote;
+window.BulletList = BulletList;
+window.HardBreak = HardBreak;
+window.Heading = Heading;
+window.ListItem = ListItem;
+window.OrderedList = OrderedList;
+window.Paragraph = Paragraph;
